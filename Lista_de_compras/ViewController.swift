a987d1ce285fe3e1ec38de1250ceb85308487a94
Eliardo <eliardosilva81@gmail.com>
2024-10-14 15:12:51 -0300

    Initial Commit

---
+++ Lista_de_compras/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lista_de_compras
+//
+//  Created by Eliardo Venancio on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
